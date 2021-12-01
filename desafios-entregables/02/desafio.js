de86d3890f82a0559dc37d4ac95e961dbc91bf5b
Marcos Peirone <marcos.peirone22@gmail.com>
2021-12-01 15:40:25 -0300

    desafio2 - Manejo de archivos - Completo

--- desafios-entregables/02/desafio.js
+++ desafios-entregables/02/desafio.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 const fs = require("fs");
 const { json } = require("stream/consumers");
 
@@ -9,28 +10,100 @@ class Contenedor {
   async getAll() {
     try {
       const contenido = await fs.promises.readFile(this.archivo, "utf-8");
+      if (!contenido) {
+        const productos = [];
+        fs.writeFileSync(this.archivo, JSON.stringify(productos));
+        return productos;
+      }
       const datos = JSON.parse(contenido);
       return datos;
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   }
-  async save (obj) {
-    const array = this.getAll().then((res) => {
-      if (res.length > 0) {
-        obj.id = res.length + 1;
-        res.push(obj);
-        return obj.id
+
+  async getById(id) {
+    try {
+      const array = await this.getAll()
+        .then((res) => res)
+        .catch((err) => {
+          throw err;
+        });
+      if (array.length <= 0) {
+        return null;
       }
-      else {
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].id === id) {
+          return array[i];
+        }
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async save(obj) {
+    try {
+      const array = await this.getAll()
+        .then((res) => res)
+        .catch((error) => {
+          throw error;
+        });
+      if (array.length <= 0) {
         obj.id = 1;
-        res.push(obj);
+        array.push(obj);
+        const data = JSON.stringify(array);
+        fs.writeFileSync(this.archivo, data, "utf-8");
+        return obj.id;
       }
-    });
+      obj.id = array.length + 1;
+      array.push(obj);
+      const data = JSON.stringify(array);
+      fs.writeFileSync(this.archivo, data, "utf-8");
+      return obj.id;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteAll() {
+    try {
+      const array = await this.getAll()
+        .then((res) => res)
+        .catch((error) => {
+          throw error;
+        });
+      if (array.length >= 1) {
+        fs.writeFileSync(this.archivo, JSON.stringify([]));
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      let array = await this.getAll()
+        .then((res) => res)
+        .catch((error) => {
+          throw error;
+        });
+      if (array.length >= 1) {
+        array = array.filter((obj) => {
+          return obj.id !== id;
+        });
+        for (let i = 0; i < array.length; i++) {
+          if (array[i].id > id) {
+            array[i].id -= 1;
+          }
+        }
+        fs.writeFileSync(this.archivo, JSON.stringify(array), "utf-8");
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
-let prueba = new Contenedor("./productos.txt");
-
-// prueba.getAll().then((res) => console.log(res[0].id));
-console.log (prueba.save({title: 'Computadora', price: 4141.45, thumbnail: 'https://webdeimagenes.com/archivo.png'}));
+module.exports = Contenedor;
--- desafios-entregables/02/test.js
+++ desafios-entregables/02/test.js
@@ -1,3 +1,36 @@
-import Contenedor from "./desafio.js";
-let prueba = new Contenedor(productos.txt);
-prueba.getAll();
+const Contenedor = require("./desafio");
+
+let test = new Contenedor("./productos.txt");
+(async () => {
+  console.log("Probamos obtener todos los datos con getAll");
+  await test.getAll().then((res) => console.log(res));
+  console.log("Ahora probamos guardar un objeto nuevo, debería devolver id");
+  await test
+    .save({
+      title: "Computadora",
+      price: 51222,
+      thumbnail: "https://webdeimagenes.com/archivo.png",
+    })
+    .then((res) => console.log(res));
+  console.log(
+    "Probamos obtener todos los objetos de nuevo para verificar que se haya guardado bien"
+  );
+  await test.getAll().then((res) => console.log(res));
+  console.log(
+    "Probamos obtener un objeto por su id. Si no existe, devuelve Null"
+  );
+  await test.getById(2).then((res) => console.log(res));
+  console.log(
+    "Probamos borrar un objeto por su id, devuelve undefined y el id de cada objeto se acomoda para que no se desordene ni se repita, por tanto otro objeto ocupará el id del objeto borrado"
+  );
+  await test.deleteById(4).then((res) => console.log(res));
+  console.log(
+    "Probamos borrar todos los objetos. Destrucción total en 3 2 1... devuelve undefined"
+  );
+  await test.deleteAll().then((res) => console.log(res));
+  console.log(
+    "Comprobamos con nuestro querido getAll que después de tanto esfuerzo, solo queda un array vacío"
+  );
+  await test.getAll().then((res) => console.log(res));
+  console.log("Fin de las pruebas");
+})();
